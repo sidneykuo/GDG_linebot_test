0e6da68b61b4b32f3685ec4bd2692292ac3b5307
sidneykuo <sidney_kuo@yahoo.com>
2026-07-27 13:28:55 +0800

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -64,17 +64,15 @@ def handle_message(event: Event):
     user_message_type = event.message.type # 使用者的訊息型態 (test / image / file / ?...)
     app.logger.info(f"收到的訊息type: {user_message_type}")
 
-     if event.message.type == "text":
-        user_message = event.message.text  # 使用者的訊息
-        app.logger.info(f"收到的訊息: {user_message}")
+    user_message = event.message.text  # 使用者的訊息
+    app.logger.info(f"收到的訊息: {user_message}")
 
-        ## 使用 GPT 生成回應
-        reply_text = ("LINEBot Test收到：" + user_message)
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_text)
-        )
+    ## 使用 GPT 生成回應
+    reply_text = ("LINEBot Test收到：" + user_message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
 
         
 # 2. 處理圖片訊息
