5f619a7e010f5b40a32b53a03e5bec923d191a47
sidneykuo <sidney_kuo@yahoo.com>
2026-07-27 13:21:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,8 @@ def handle_message(event: Event):
     # 2026/7/27 新增by Sidney，想查看message.type
     user_message_type = event.message.type # 使用者的訊息型態 (test / image / file / ?...)
     app.logger.info(f"收到的訊息type: {user_message_type}")
-        
+
+     if event.message.type == "text":
         user_message = event.message.text  # 使用者的訊息
         app.logger.info(f"收到的訊息: {user_message}")
 
