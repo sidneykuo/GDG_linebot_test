8849a72cbe204a9f686c10303455c0de0473f7d5
sidneykuo <sidney_kuo@yahoo.com>
2026-08-03 11:14:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -124,8 +124,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_text_message(event):
     details = get_source_info(event)
-    user_info = f"{details['user_name']} ({details['user_id']})" if details['user_name'] else details['user_id']
+    user_info  = f"{details['user_name']} ({details['user_id']})" if details['user_name'] else details['user_id']
     group_info = f"{details['group_name']} ({details['group_id']})" if details['group_name'] else (details['group_id'] or "私訊")
+    room_info  = f"{details['room_id']}" if details['room_id'] else ("私訊")
     room_id = f"{details['room_id']}"
     user_message_type = event.message.type # 使用者的訊息型態 (此處應為text)
     message_id = event.message.id          # 使用者的訊息ID
